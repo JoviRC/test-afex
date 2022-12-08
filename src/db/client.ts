be30c662fd456ee8f4c20de4564eb6d565c959fd
jovirc <johanrc95@gmail.com>
2022-12-07 23:41:10 -0300

    update env

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,19 +1,18 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+import { config } from 'dotenv';
 
-dotenv.config({ path: `${__dirname}/../../../.env` });
+config({ path: resolve(__dirname, '../../.env') });
 
 const dynamoClient = new DynamoDBClient({
   region: 'us-east-1',
-  endpoint: process.env.DYNAMODB_ENDPOINT,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
-console.log('apikey', process.env.AWS_ACCESS_KEY_ID);
 const docClient = DynamoDBDocumentClient.from(dynamoClient);
 
 export default docClient;
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,7 @@
 import { UserController } from './users.controller';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ConfigModule } from '@nestjs/config';
-import { environment } from '../enviroments';
-
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: environment[process.env.NODE_ENV] || '.env',
-      isGlobal: true,
-    }),
-  ],
   controllers: [UserController],
   providers: [UsersService],
 })
